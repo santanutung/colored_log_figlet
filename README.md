308aeac8f5f9700a2e895ece72299ade66c0d3ae
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 <santanutung>
2022-05-03 21:51:40 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+python -m venv --system-site-packages .\venv
+
+.\venv\Scripts\activate
+
+python -m venv --system-site-packages .\venv
+pip install --upgrade pip
+
+pip list  # show packages installed within the virtual environment
+
+ <!-- pip install requirements.txt -->
+ pip install -r requirements.txt  install 
---
+++ index.py
@@ -0,0 +1,15 @@
+from termcolor import colored
+from pyfiglet import Figlet
+
+
+
+
+f=Figlet(font='standard')
+# f=Figlet(font='banner3-D')
+# f=Figlet(font='slant')
+# f=Figlet(font='digital')
+# f=Figlet(font='isometric1')
+# f=Figlet(font='dotmatrix')
+# f=Figlet(font='alligator')
+# print(colored(f.renderText('Santanu tung')))
+print(colored(f.renderText('Eid Mubarak \n And \n Happy \n Akshay Tritiya'),'red'))
